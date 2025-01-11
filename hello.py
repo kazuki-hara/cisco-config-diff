3f1262c6bc100172b55828ebbade16a2045d1a9c
原和希 <kazuki@harakazukinoMac-mini.local>
2025-01-12 01:27:25 +0900

    first commit

---
+++ hello.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from cisco-conf-diff!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ main.py
@@ -0,0 +1,5 @@
+import json
+import difflib
+
+def convert_cisco_ios_config_to_dict():
+    return {}
